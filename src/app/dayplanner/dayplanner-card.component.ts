51230450a6220c0e1687b610bdde7e3be5fb33bc
Filipe Silva <filipematossilva@gmail.com>
2018-11-10 12:35:39 +0000

    build: use the sm selector

--- src/app/dayplanner/dayplanner-card.component.ts
+++ src/app/dayplanner/dayplanner-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dayplanner',
+  selector: 'sm-dayplanner-card',
   templateUrl: './dayplanner-card.component.html',
   styleUrls: ['./dayplanner-card.component.css']
 })
