ddc77d2efafc958d339d543b24d9d3f3a03e8814
Filipe Silva <filipematossilva@gmail.com>
2018-12-23 23:37:31 +0000

    feat: add tooltips to buttons

--- src/app/dayplanner/dayplanner.module.ts
+++ src/app/dayplanner/dayplanner.module.ts
@@ -7,12 +7,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {
+  MatTooltipModule,
+  MatTooltipDefaultOptions,
+  MAT_TOOLTIP_DEFAULT_OPTIONS
+} from '@angular/material/tooltip';
 
 import { DayplannerRoutingModule } from './dayplanner-routing.module';
 import { DayplannerCardComponent } from './card/dayplanner-card.component';
 import { DayplannerItemComponent } from './item/dayplanner-item.component';
 import { TimeNumberPipe } from './time-number/time-number.pipe';
 import { DayplannerItemEditComponent } from './item-edit/dayplanner-item-edit.component';
+
+
+/** Custom options the configure the tooltip's default show/hide delays. */
+export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 500,
+  hideDelay: 500,
+  touchendHideDelay: 500,
+};
 
 @NgModule({
   imports: [
@@ -25,12 +38,16 @@ import { DayplannerItemEditComponent } from './item-edit/dayplanner-item-edit.co
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatTooltipModule,
   ],
   declarations: [
     DayplannerCardComponent,
     DayplannerItemComponent,
     TimeNumberPipe,
     DayplannerItemEditComponent,
-  ]
+  ],
+  providers: [
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults }
+  ],
 })
 export class DayplannerModule { }
