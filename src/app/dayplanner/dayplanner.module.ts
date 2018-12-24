42dad4fe247aeddede33f5bbe5f22abf75404173
Filipe Silva <filipematossilva@gmail.com>
2018-12-24 01:07:34 +0000

    fix: validate new item form

--- src/app/dayplanner/dayplanner.module.ts
+++ src/app/dayplanner/dayplanner.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -32,6 +32,7 @@ export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
     CommonModule,
     DayplannerRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
--- src/app/dayplanner/item-edit/dayplanner-item-edit.component.ts
+++ src/app/dayplanner/item-edit/dayplanner-item-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef, 
 import { DayplannerItem, RawDayplannerItem } from '../dayplanner-models';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
 import { parseItemString, itemToItemString } from '../date-number-utils';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sm-dayplanner-item-edit',
@@ -15,13 +16,12 @@ import { parseItemString, itemToItemString } from '../date-number-utils';
           <input
             #itemInput
             matInput
+            [formControl]="itemFormControl"
             type="text"
-            id="itemString"
-            name="itemString"
-            placeholder="New item"
-            [(ngModel)]="itemString"
+            placeholder="I intend to..."
             (blur)="exit.emit()"
           >
+          <mat-error>You must enter something</mat-error>
         </mat-form-field>
       </form>
     </mat-list-item>
@@ -43,25 +43,27 @@ export class DayplannerItemEditComponent implements OnInit {
   @Input() collection: AngularFirestoreCollection<RawDayplannerItem>;
   @Output() exit: EventEmitter<void> = new EventEmitter();
   @ViewChild('itemInput') itemInputField: ElementRef;
-  itemString: string;
+  itemFormControl = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit() {
     if (this.item) {
-      this.itemString = itemToItemString(this.item);
+      this.itemFormControl.setValue(itemToItemString(this.item));
     }
     this.itemInputField.nativeElement.focus();
   }
 
   onSubmit() {
-    const rawItem = parseItemString(this.itemString);
-    if (this.item) {
-      this.collection.doc(this.item.id).update(rawItem);
-    } else {
-      this.collection.add(rawItem);
+    if (this.itemFormControl.valid) {
+      const rawItem = parseItemString(this.itemFormControl.value);
+      if (this.item) {
+        this.collection.doc(this.item.id).update(rawItem);
+      } else {
+        this.collection.add(rawItem);
+      }
+      this.emitExit();
     }
-    this.emitExit();
   }
 
   @HostListener('document:keyup.escape', ['$event'])
