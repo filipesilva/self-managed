39299cc4cdabc776d6f606b16ada837ba32521e3
Filipe Silva <filipematossilva@gmail.com>
2018-12-28 13:30:42 +0000

    ci: add deploy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBco5tAAzmMS35TJkQj7dUCa1e2MptJYmE',
+    authDomain: 'self-managed.firebaseapp.com',
+    databaseURL: 'https://self-managed.firebaseio.com',
+    projectId: 'self-managed',
+    storageBucket: 'self-managed.appspot.com',
+    messagingSenderId: '897927229701'
+  }
 };
