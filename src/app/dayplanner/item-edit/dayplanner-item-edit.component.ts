3f3aa301330a3c13a12fbd3b69837edbcab7c737
Filipe Silva <filipematossilva@gmail.com>
2018-12-31 17:25:54 +0000

    style: use custom list items

--- src/app/dayplanner/item-edit/dayplanner-item-edit.component.ts
+++ src/app/dayplanner/item-edit/dayplanner-item-edit.component.ts
@@ -9,33 +9,30 @@ import {
   HostListener,
 } from '@angular/core';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 import { DayplannerItem, RawDayplannerItem } from '../dayplanner-item';
-import { Keybind } from '../keybind.decorator';
 
 
 @Component({
   selector: 'sm-dayplanner-item-edit',
   template: `
-    <mat-list-item>
-      <form class="dayplanner-form" (ngSubmit)="onSubmit()">
-        <mat-form-field
-          class="dayplanner-full-width"
-          [floatLabel]="'never'"
+    <form class="dayplanner-form" (ngSubmit)="onSubmit()">
+      <mat-form-field
+        class="dayplanner-full-width"
+        [floatLabel]="'never'"
+      >
+        <input
+          #itemInput
+          matInput
+          [formControl]="itemFormControl"
+          type="text"
+          placeholder="I intend to..."
+          (blur)="emitExit()"
         >
-          <input
-            #itemInput
-            matInput
-            [formControl]="itemFormControl"
-            type="text"
-            placeholder="I intend to..."
-            (blur)="emitExit()"
-          >
-          <mat-error>You must enter something</mat-error>
-        </mat-form-field>
-      </form>
-    </mat-list-item>
+        <mat-error>You must enter something</mat-error>
+      </mat-form-field>
+    </form>
   `,
   styles: [`
     .dayplanner-form {
